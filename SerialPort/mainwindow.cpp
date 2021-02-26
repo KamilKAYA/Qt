c1396430466fb040cfe4cd1409c23f2e98be5587
Kamil KAYA <kamil@kamilkaya.com.tr>
2021-02-27 00:03:32 +0300

    haberleşme testleri yapıldı.

--- SerialPort/mainwindow.cpp
+++ SerialPort/mainwindow.cpp
@@ -1,19 +1,50 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
-#include "qtserialclass.h"
+#include "arduinoserial.h"
+#include <QDebug>
 
-QtSerialClass mySerialPort;
+ArduinoSerial mySerialPort;
 
-MainWindow::MainWindow(QWidget *parent) :
-    QMainWindow(parent),
-    ui(new Ui::MainWindow)
+void getPortNames();
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    mySerialPort.PortName="COM4";
-    mySerialPort.BaudRate=9600;
+
+    mySerialPort.getPortNames();
+
 }
+
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+
+void MainWindow::on_listPorts_clicked()
+{
+    mySerialPort.getPortNames();
+    ui->ports->clear();
+    for(unsigned int i=0; i<mySerialPort.availablePortNames.size; i++)ui->ports->addItem(mySerialPort.availablePortNames.name[i]);
+}
+
+void MainWindow::on_connectToArduino_clicked()
+{
+    QString selectedPortName=ui->ports->itemText(ui->ports->currentIndex());
+
+    mySerialPort.Port.setPortName(selectedPortName);
+    mySerialPort.connect();
+}
+
+void MainWindow::on_ports_activated(const QString &arg1)
+{
+
+}
+
+void MainWindow::on_send_clicked()
+{
+    mySerialPort.write();
 }
--- SerialPort/mainwindow.h
+++ SerialPort/mainwindow.h
@@ -3,20 +3,29 @@
 
 #include <QMainWindow>
 
-namespace Ui {
-class MainWindow;
-}
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
 
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
 
 public:
-    explicit MainWindow(QWidget *parent = nullptr);
+    MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
+
+private slots:
+    void on_listPorts_clicked();
+
+    void on_connectToArduino_clicked();
+
+    void on_ports_activated(const QString &arg1);
+
+    void on_send_clicked();
 
 private:
     Ui::MainWindow *ui;
 };
-
 #endif // MAINWINDOW_H
